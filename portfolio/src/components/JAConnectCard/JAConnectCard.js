64c62ae5459e0b694dfa086cd6467f0b700eddc5
David Walton <davidwalton6772@gmail.com>
2020-10-16 16:01:50 -0500

    added icons on bottom of cards

--- portfolio/src/components/JAConnectCard/JAConnectCard.js
+++ portfolio/src/components/JAConnectCard/JAConnectCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { useSpring, animated } from 'react-spring'
 import './JAConnectCard.css'
+import CardIcons from "../CardIcons/CardIcons";
 
 const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
@@ -9,11 +10,14 @@ const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg)
 export default function JAConnectCard() {
     const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
     return (
-        <animated.div
-            className="jaconnectCard"
-            onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-            onMouseLeave={() => set({ xys: [0, 0, 1] })}
-            style={{ transform: props.xys.interpolate(trans) }}
-        />
+        <div className="cardDiv">
+            <animated.div
+                className="jaconnectCard"
+                onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+                onMouseLeave={() => set({ xys: [0, 0, 1] })}
+                style={{ transform: props.xys.interpolate(trans) }}
+            />
+            <CardIcons />
+        </div>
     )
 }
